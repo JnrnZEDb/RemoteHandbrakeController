360fa90547940cda16579cc8907950eb7bb4d5ea
Daniel Shinevar <danshinevar@gmail.com>
2018-01-09 21:18:53 -0600

    Initial commit

---
+++ RemoteHandbrakeController/RemoteHandbrakeController/EncodeWindow.xaml.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Configuration;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace RemoteHandbrakeController
+{
+	/// <summary>
+	/// Interaction logic for EncodeWindow.xaml
+	/// </summary>
+	public partial class EncodeWindow : Window
+	{
+		private List<FileInfo> _lstFilesToEncode = new List<FileInfo>();
+		public List<FileInfo> lstFilesToEncode
+		{
+			get
+			{
+				return _lstFilesToEncode;
+			}
+			set
+			{
+				_lstFilesToEncode = value;
+			}
+		}
+
+		public EncodeWindow(List<FileInfo> lstFiles)
+		{
+			lstFilesToEncode = lstFiles;
+			InitializeComponent();
+			lstBoxFiles.ItemsSource = lstFilesToEncode;
+		}
+
+		private void BtnStartEncode_Click(object sender, RoutedEventArgs e)
+		{
+			txtOutput.Text += "Hello\n";
+		}
+	}
+}
---
+++ RemoteHandbrakeController/RemoteHandbrakeController/MainWindow.xaml.cs
@@ -0,0 +1,112 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+using System.Windows;
+using System.Windows.Controls;
+
+
+namespace RemoteHandbrakeController
+{
+	/// <summary>
+	/// Interaction logic for MainWindow.xaml
+	/// </summary>
+	public partial class MainWindow : Window
+	{
+		List<FileInfo> lstFilesToBeEncoded = new List<FileInfo>();
+
+		public MainWindow()
+		{
+			InitializeComponent();
+		}
+
+		private void BtnMovies_Clicked(object sender, RoutedEventArgs e)
+		{
+			ListDirectory(treeFiles, "H:\\Movies");
+		}
+
+		private void BtnTV_Clicked(object sender, RoutedEventArgs e)
+		{
+			ListDirectory(treeFiles, "H:\\TV Shows");
+		}
+
+		private void BtnAnime_Clicked(object sender, RoutedEventArgs e)
+		{
+			ListDirectory(treeFiles, "H:\\Anime");
+		}
+
+		private void BtnEncode_Click(object sender, RoutedEventArgs e)
+		{
+			lstFilesToBeEncoded.Clear();
+			FindChecked((TreeViewItem)treeFiles.Items.GetItemAt(0));
+			EncodeWindow wndEncode = new EncodeWindow(lstFilesToBeEncoded);
+			wndEncode.Show();
+		}
+
+		/// <summary>
+		/// Fills TreeView with files from specified directory
+		/// </summary>
+		/// <param name="treeView"></param>
+		/// <param name="path"></param>
+		private void ListDirectory(TreeView treeView, string path)
+		{
+			treeView.Items.Clear();
+			var rootDirectoryInfo = new DirectoryInfo(path);
+			treeView.Items.Add(CreateDirectoryNode(rootDirectoryInfo));
+		}
+
+		/// <summary>
+		/// Recursively builds treeView
+		/// </summary>
+		/// <param name="directoryInfo"></param>
+		/// <returns></returns>
+		private static TreeViewItem CreateDirectoryNode(DirectoryInfo directoryInfo)
+		{
+			var directoryNode = new TreeViewItem
+			{
+				Header = directoryInfo.Name
+			};
+			foreach (var directory in directoryInfo.GetDirectories())
+				directoryNode.Items.Add(CreateDirectoryNode(directory));
+			foreach (var file in directoryInfo.GetFiles("*.mkv"))
+			{
+				directoryNode.Items.Add(new TreeViewItem
+				{
+					Header = new CheckBox()
+					{
+						Content = file.Name,
+						DataContext = file
+					},
+					
+				});
+			}
+				
+			return directoryNode;
+		}
+
+		/// <summary>
+		/// Gets all checked files from TreeViewItem
+		/// </summary>
+		/// <param name="treeViewFile"></param>
+		private void FindChecked(TreeViewItem treeViewFile)
+		{
+			foreach (TreeViewItem file in treeViewFile.Items)
+			{
+				if (file.Header is CheckBox fileCheckBox)
+				{
+					fileCheckBox = (CheckBox)file.Header;
+					if (fileCheckBox.IsChecked.Value)
+					{
+						FileInfo checkedFile = (FileInfo)fileCheckBox.DataContext;
+						lstFilesToBeEncoded.Add(checkedFile);
+						//Console.WriteLine(checkedFile.FullName);
+					}
+				}
+				FindChecked(file);
+			}
+		}
+
+	}
+}
